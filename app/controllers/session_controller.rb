29b34b49989dbe77c34e8cafa4de55e41136b817
51103220 <rewandedo1009@gmail.com>
2015-09-19 03:30:29 +0700

    Complete SignInUp

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -5,6 +5,7 @@ class SessionController < ApplicationController
 		user = User.find_by(email: params[:session][:email].downcase)
 		if user && user.authenticate(params[:session][:password])
 			log_in user
+			params[:session][:remember_me] == '1' ? remember(user) : forget(user)
 			redirect_to controller: "home_page", action: "home" 
 		else
 			flash.now[:danger] = 'Invalid email or password'
@@ -12,7 +13,7 @@ class SessionController < ApplicationController
 		end
 	end
 	def destroy
-		log_out
+		log_out if logged_in?
 		redirect_to root_url
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150918095827) do
+ActiveRecord::Schema.define(version: 20150918192832) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "post_id",    limit: 4
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20150918095827) do
     t.string   "password_digest", limit: 255
     t.string   "avatar_url",      limit: 255
     t.string   "real_name",       limit: 255
+    t.string   "remember_digest", limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
