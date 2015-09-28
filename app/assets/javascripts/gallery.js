812a67ffe2687bf88d6c08674dddec37eb64c7ae
51103220 <rewandedo1009@gmail.com>
2015-09-28 22:35:12 +0700

    Nearly Pakuta

--- app/assets/javascripts/gallery.js
+++ app/assets/javascripts/gallery.js
@@ -1,30 +1,38 @@
 $(document).ready(function(){
-
+	$(".close-button").click(function(e){
+		e.preventDefault(); 
+		$("body").removeAttr("style");
+		$('.gallery #review-img').hide();
+		$('.gallery #review-img').removeAttr("id","review-img");
+		//$(".gallery .closeImg-btn").hide();
+		$(".gallery .close-button").hide();
+		$(".gallery .nextImg-btn").hide();
+		$(".gallery .prevImg-btn").hide();
+	});
 	$(".gallery .gl-item").on("click", function(event){
 		event.preventDefault();
-		$(".gallery .closeImg-btn").show(); //this is the dark background behind the images in "review mode"
+		$("a.close-button").css("display","block");
+		//$(".gallery .closeImg-btn").show(); //this is the dark background behind the images in "review mode"
 		$(".gallery .nextImg-btn").show();
 		$(".gallery .prevImg-btn").show();
 		
+
 		if($(".gallery ul li").index($(this))== 0) { //if the clicked picture is the first picture, then hide the "prev image" button.
 			$(".gallery .prevImg-btn").hide();
-		}
+	}
 		if($(".gallery ul li").index($(this)) == $(".gallery ul li").length - 1 ) { //if the clicked picture is the last picture, then hide the "next image" button.
 			$(".gallery .nextImg-btn").hide();
-		}
+	}
 		$(this).find('.gl-preview').attr("id", "review-img"); //add the review-img -Id to the clicked image (makes the image larger) 
-		//$container.packery();
+		//alert($(this).find('.gl-preview').html());
+		$("body").css("overflow","hidden");
 		$(this).find('.gl-preview').show();
 	});
-	
-	$(".gallery .closeImg-btn").on("click", function(){ //when the black background is clicked, the user will be taken back to the "gallery view" and all the elements associated with the "review mode" are hidden
-		$('.gallery #review-img').hide();
-		$('.gallery #review-img').removeAttr("id","review-img");
-		$(".gallery .closeImg-btn").hide();
-		$(".gallery .nextImg-btn").hide();
-		$(".gallery .prevImg-btn").hide();
-		//$container.packery('reloadItems');
-		//$container.packery();
+	$("ul.gallery .gl-item .gl-preview .gl-preview-wrapper").click(function(){ //when the black background is clicked, the user will be taken back to the "gallery view" and all the elements associated with the "review mode" are hidden
+		$(".close-button").click();
+	});
+	$(".cm-title a").click(function(){
+		window.location.replace($(this).attr('href'));
 	});
 });
 //IMAGE NAVIGATION BUTTONS
@@ -51,11 +59,24 @@ $(document).ready(function(){
 		$(".gallery .nextImg-btn").show(); 
 		if($("ul.gallery .gl-item").index($(activeParentDiv)) - 1 == 0) { //if the next picture is the first picture of the container, then hide the "previous image" button.
 			$(".gallery .prevImg-btn").hide();
-			$(".gallery .nextImg-btn").show();
-		}
-		$(activeImage).hide();
+		$(".gallery .nextImg-btn").show();
+	}
+	$(activeImage).hide();
 		$(activeImage).removeAttr("id", "review-img"); //remove the "review-img"-ID from the picture
 		$(activeParentDiv).prev("li").find(".gl-preview").attr("id", "review-img"); //find the picture next to the current picture and add the review-img-ID to it, i.e. next pictuer will be shown.
 		$(activeParentDiv).prev("li").find(".gl-preview").show();
+	});
+	$("body").keydown(function(e) {
+	  if(e.keyCode == 37) { // left
+	  	if ($(".gallery .prevImg-btn").css('display') != 'none')
+	  		$(".gallery .prevImg-btn").click();
+	  }
+	  if(e.keyCode == 27) { // left
+	  		$(".close-button").click();
+	  }
+	  else if(e.keyCode == 39) { // right
+	  	if ($(".gallery .nextImg-btn").css('display') != 'none')
+	  		$(".gallery .nextImg-btn").click();
+	  }
 	});
 });
